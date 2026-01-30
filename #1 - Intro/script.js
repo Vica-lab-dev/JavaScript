d8ca73be9027b6c52e9eca412d278ab2762e591a
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-30 19:49:37 +0100

    Add string concatenation example for full name

--- #1 - Intro/script.js
+++ #1 - Intro/script.js
@@ -9,3 +9,11 @@ let height = 186.3;
 let isProgrammer = true;
 
 console.log(name, age, height, isProgrammer);
+
+let firstName = "Viktor";
+
+let lastName = "Maletkovic";
+
+let result = firstName+" "+lastName
+
+console.log(result)
