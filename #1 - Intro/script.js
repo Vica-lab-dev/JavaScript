e82e2803b209b38f9367babf23936671afd05ebf
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-01-30 20:07:04 +0100

    Homework 1 - Add let array and demonstrate push method

--- #1 - Intro/script.js
+++ #1 - Intro/script.js
@@ -1,19 +1,14 @@
 
 
 let name = "Viktor";
-
 let age = 27;
-
 let height = 186.3;
-
 let isProgrammer = true;
 
 console.log(name, age, height, isProgrammer);
 
 let firstName = "Viktor";
-
 let lastName = "Maletkovic";
-
 let result = firstName+" "+lastName
 
 console.log(result)
@@ -23,3 +18,8 @@ let secondNumber = 18;
 let count = firstNumber+secondNumber;
 
 console.log(count)
+
+let array = ["Computer", "Keyboard"];
+array.push("Mouse");
+
+console.log(array);
