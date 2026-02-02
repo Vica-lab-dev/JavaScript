3516294be284cfd8b83a9d809c62ba1a1b3acc5c
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-02 20:21:43 +0100

    Add else if branch for site content

--- #3 - If/exercise.js
+++ #3 - If/exercise.js
@@ -4,7 +4,10 @@ if(content == "video")
 {
     console.log("https://www.youtube.com/watch?v=oLKMuH3SiRA");
 }
-
+else if(content == "site")
+{
+    console.log("itmentorstva.com")
+}
 else
 {
     console.log("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxngkaPCwZS-pN67h-JfCygadqtqu7g2N7NQ&s");
