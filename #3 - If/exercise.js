65a0b114188f8179f248fe50492a5e7a5c1687f6
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-02 20:17:52 +0100

    Load video or image URL based on content type

---
+++ #3 - If/exercise.js
@@ -0,0 +1,11 @@
+let content = "image";
+
+if(content == "video")
+{
+    console.log("https://www.youtube.com/watch?v=oLKMuH3SiRA");
+}
+
+else
+{
+    console.log("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxngkaPCwZS-pN67h-JfCygadqtqu7g2N7NQ&s");
+}
