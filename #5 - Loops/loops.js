78a8ebd3cd1a4ce06023e36d9d34363c904244b9
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-04 20:07:20 +0100

    feat: add for loop to log numbers from 0 to 99

---
+++ #5 - Loops/loops.js
@@ -0,0 +1,9 @@
+/**let i = 0 => odakle krece petlja.
+ * i<100 => uslov, do kada ce petlja da radi.
+ * i++ => nakon svakog ponavljanja povecaj i za 1...
+ */
+
+for(let i= 0; i < 100; i++)
+{
+    console.log(i);
+}
