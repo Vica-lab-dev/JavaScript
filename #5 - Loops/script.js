8b3ea50984ac34a09d9a3d9cbff79a02bd5404ef
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-04 19:22:52 +0100

    feat: add lowercase normalization and check if name starts with a vowel

---
+++ #5 - Loops/script.js
@@ -0,0 +1,16 @@
+let name = "admIn";
+name = name.toLowerCase();
+
+if(name == "admin")
+{
+    console.log("You are an administrator!");
+}
+else
+{
+    console.log("You are not an administrator!");
+}
+
+if(name[0] == "a")
+{
+    console.log("Name starts with vowel!");
+}
