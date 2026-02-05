d359cd49d8a8eff43c4a8d809bc837e379ef6856
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-05 10:06:28 +0100

    feat: classify temperature as cold, moderate, or hot

---
+++ #4 - If switch/exercises.js
@@ -0,0 +1,14 @@
+let temperature = -1;
+
+if(temperature < 15)
+{
+    console.log("It's cold!");
+}
+else if(temperature >= 15 && temperature <= 25)
+{
+    console.log("Moderate temperature!");
+}
+else
+{
+    console.log("It's Hot!");
+}
