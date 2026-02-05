b21ab69b21e900df60c8b3bb0fa5705897b42139
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-05 10:13:18 +0100

    feat: greet based on time of day with case-insensitive check

--- #4 - If switch/exercises.js
+++ #4 - If switch/exercises.js
@@ -12,3 +12,22 @@ else
 {
     console.log("It's Hot!");
 }
+
+let timeOfDay = "Night";
+
+if(timeOfDay.toLowerCase() === "morning")
+{
+    console.log("Good Morning!");
+}
+else if(timeOfDay.toLowerCase() === "afternoon")
+{
+    console.log("Good afternoon!");
+}
+else if(timeOfDay.toLowerCase() === "evening")
+{
+    console.log("Good evening!");
+}
+else
+{
+    console.log(timeOfDay);
+}
