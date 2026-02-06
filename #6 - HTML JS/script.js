bf75617973db29b91c5f8b50e17bc4840682cfc5
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-06 21:50:16 +0100

    Print prices from cart array

--- #6 - HTML JS/script.js
+++ #6 - HTML JS/script.js
@@ -4,3 +4,29 @@ for(let product in products)
 {
     console.log(products[product]);
 }
+
+let cart =
+    [
+        {
+            name: "Bread",
+            price: 99,
+            amount: 1
+        },
+
+        {
+            name: "Onion",
+            price: 150,
+            amount: 3
+        },
+
+        {
+          name: "Milk",
+          price: 250,
+          amount: 1
+        }
+    ]
+
+for(let price in cart)
+{
+    console.log(cart[price]["price"])
+}
