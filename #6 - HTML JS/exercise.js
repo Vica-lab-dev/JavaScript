77b66137ed571710f02e1415958c81831b42b872
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-06 23:07:48 +0100

    feat: add grade evaluation alerts

--- #6 - HTML JS/exercise.js
+++ #6 - HTML JS/exercise.js
@@ -1,12 +1,35 @@
-let names = ["admin", "administrator", 'viktor'];
+// let names = ["admin", "administrator", 'viktor'];
+//
+// let name= prompt("Enter your name").toLowerCase();
+//
+// if(names.includes(name))
+// {
+//     alert("Welcome "+name);
+// }
+// else
+// {
+//     alert("You do not have access to this site!");
+// }
 
-let name= prompt("Enter your name").toLowerCase();
+let grade = prompt("Enter grade 0-100");
 
-if(names.includes(name))
+if(grade < 100 && grade >= 90)
 {
-    alert("Welcome "+name);
+    alert("Excellent!");
+}
+else if(grade < 90 && grade >= 75)
+{
+    alert("Good!");
+}
+else if(grade < 75 && grade >= 50)
+{
+    alert("Average!");
+}
+else if(grade < 50 && grade >= 0)
+{
+    alert("Fail!");
 }
 else
 {
-    alert("You do not have access to this site!");
+    alert("Try again!");
 }
