f836c2c1f6e39e72e6a20a504b344790e7f84e75
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-08 20:22:40 +0100

    feat: allow user to set background color via prompt

--- #7 - DOM/script.js
+++ #7 - DOM/script.js
@@ -1,6 +1,7 @@
 let name = prompt("Enter your name");
 let color = prompt("Enter text color");
+let bgColor = prompt("Enter background color");
 let marqueeElement = document.getElementById("name");
 marqueeElement.style.color = color;
-marqueeElement.style.backgroundColor = "black";
+marqueeElement.style.backgroundColor = bgColor;
 marqueeElement.innerText = name;
