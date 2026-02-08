f38541afe49e0a5b8950c928cc4cca7e0cbf63d0
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-08 20:21:12 +0100

    feat: allow user to set name and text color via prompt

--- #7 - DOM/script.js
+++ #7 - DOM/script.js
@@ -1,5 +1,6 @@
+let name = prompt("Enter your name");
+let color = prompt("Enter text color");
 let marqueeElement = document.getElementById("name");
-
-marqueeElement.style.color = "red";
+marqueeElement.style.color = color;
 marqueeElement.style.backgroundColor = "black";
-marqueeElement.innerText = "Dev";
+marqueeElement.innerText = name;
