fa864888f26a7a18af048f2e16cb322fdb38c58b
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-08 20:41:42 +0100

    style: update element colors using getElementsByClassName

---
+++ #7 - DOM/selectors.js
@@ -0,0 +1,13 @@
+/**
+ * -getElementById -> prosledimo ID, dobijemo element
+ * geElementByClassName -> prolsedimo klasu i dobijemo array sa elementima
+ *
+ */
+let elements = document.getElementsByClassName("description");
+elements[0].style.color = "red";
+elements[1].style.color = "blue";
+
+let hTitle = document.getElementsByClassName("title")
+hTitle[0].style.color = "green";
+
+
