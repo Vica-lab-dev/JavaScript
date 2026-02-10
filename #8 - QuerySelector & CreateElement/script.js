b06ee0b9b8715d69f6a978c09ea8b5500e624221
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-10 09:18:19 +0100

    feat: style product prices based on value using DOM selectors

--- #8 - QuerySelector & CreateElement/script.js
+++ #8 - QuerySelector & CreateElement/script.js
@@ -8,8 +8,21 @@ document.querySelector(".subtitle").style.color = "green";
 document.getElementsByClassName("subtitle2")[0].style.color = "blue";
 
 let products = document.querySelectorAll(".product");
+let prices = document.querySelectorAll(".productPrice");
 
 for(let product of products)
 {
-    product.style.color = "red";
+    product.style.color = "#616161";
 }
+
+for(let price of prices)
+{
+    if(price.textContent >= 5000)
+    {
+        price.style.color = "red";
+    }
+    else
+    {
+        price.style.color = "green";
+    }
+    }
