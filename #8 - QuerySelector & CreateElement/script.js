fdbbcb2fef2f7088dbaf43cfae9ffa23c160c685
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-10 08:54:46 +0100

    feat: practice DOM selection with querySelector and class selectors

---
+++ #8 - QuerySelector & CreateElement/script.js
@@ -0,0 +1,15 @@
+/**
+ * -querySelector -> "#name" ili ".name" - vraca jedan element
+ * -querySelectorAll -> "#name" ili ".name" - vraca array sa elementima
+ */
+
+document.querySelector("#title").style.color = "red";
+document.querySelector(".subtitle").style.color = "green";
+document.getElementsByClassName("subtitle2")[0].style.color = "blue";
+
+let products = document.querySelectorAll(".product");
+
+for(let product in products)
+{
+    products[product].style.color = "red";
+}
