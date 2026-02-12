5fda904ef97c2f4ba8a11b627c5f67b8dd270056
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-12 21:37:10 +0100

    Add h2 and p elements to div using createElement

---
+++ #8 - QuerySelector & CreateElement/extraExercise5.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("box");
+
+let h = document.createElement("h2");
+h.textContent = "Title";
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Description";
+
+div.appendChild(h);
+div.appendChild(paragraph);
