bf828639b03ba62663fa468491a87258e2ece3c9
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-12 21:51:02 +0100

    Add h3 and paragraph elements to div using createElement

---
+++ #8 - QuerySelector & CreateElement/extraExercise7.js
@@ -0,0 +1,10 @@
+let div = document.querySelector(".card");
+
+let h = document.createElement("h3");
+h.textContent = "Card";
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Card description";
+
+div.appendChild(h);
+div.appendChild(paragraph)
