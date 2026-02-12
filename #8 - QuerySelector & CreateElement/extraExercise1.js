eac245d7d14b8494e397d50ee410d70edd5a9903
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-12 20:16:07 +0100

    Add paragraph element to div using createElement

---
+++ #8 - QuerySelector & CreateElement/extraExercise1.js
@@ -0,0 +1,7 @@
+let div = document.getElementById("main");
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Hello!";
+paragraph.style.color = "red";
+
+div.appendChild(paragraph);
