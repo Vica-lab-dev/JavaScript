fe81877c42c01f23df29d0ba7cc9319225df4dea
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-12 20:53:01 +0100

    Display message based on number comparison

---
+++ #8 - QuerySelector & CreateElement/extraExercise3.js
@@ -0,0 +1,12 @@
+let p = document.getElementById("paragraph");
+
+number= 0
+
+if(number <= 10)
+{
+    p.textContent = "Number is less than or equal to 10!";
+}
+else
+{
+    p.textContent = "Number is greater than 10!";
+}
