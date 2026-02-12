9b1a5fe28b93b3ccb1bda22112155f5a2546bbfc
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-12 23:16:52 +0100

    Add blue paragraph to box

---
+++ #8 - QuerySelector & CreateElement/extraExercise15.js
@@ -0,0 +1,7 @@
+let div = document.querySelector(".box");
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Hello!";
+paragraph.style.color = "blue";
+
+div.appendChild(paragraph)
