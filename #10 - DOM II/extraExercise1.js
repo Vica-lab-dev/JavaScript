875ad160d9746dcb40c7593f1a28b16fe41bd439
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-18 16:23:40 +0100

    feat: add welcome paragraph dynamically to main section

---
+++ #10 - DOM II/extraExercise1.js
@@ -0,0 +1,7 @@
+let div = document.querySelector("#main");
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Welcome!";
+paragraph.style.fontSize = "18px";
+
+div.appendChild(paragraph);
