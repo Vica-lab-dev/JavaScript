b4a79bb8352b48e1a267d823480915108edd4617
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-21 14:11:23 +0100

    feat: add age validation function to check if person is adult

--- #11 - Functions/functions.js
+++ #11 - Functions/functions.js
@@ -11,3 +11,16 @@ function online()
 }
 
 online();
+
+function isAudlt(age) {
+    if (age >= 18) {
+        console.log("This person is adult!");
+    }
+    else
+    {
+        console.log("This person is not adult!");
+    }
+}
+
+isAudlt(15);
+isAudlt(18);
