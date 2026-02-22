7ff0846b2eacae6369dc5ea19f2db5ca87aa37e1
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-22 17:33:31 +0100

    feat: add printEven function to log even numbers from an array

---
+++ #11 - Functions/extraExercise5.js
@@ -0,0 +1,15 @@
+function printEven(array)
+{
+    for(let i = 0; i < array.length; i++)
+    {
+        if(array[i] % 2 === 0)
+        {
+            console.log(array[i]);
+        }
+    }
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+printEven(numbers);
+
