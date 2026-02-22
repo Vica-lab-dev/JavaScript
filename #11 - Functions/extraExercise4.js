7cac1435f60c3a0123ba00c85d405cce0b909ec6
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-02-22 17:36:48 +0100

    feat: add printArray function to log all elements of an array

---
+++ #11 - Functions/extraExercise4.js
@@ -0,0 +1,11 @@
+function printArray(array)
+{
+    for(let i = 0; i < array.length; i++)
+    {
+        console.log(array[i]);
+    }
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+printArray(numbers);
