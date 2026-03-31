f7e001aaf666cfca55b0042841e5aca9ad125c98
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-03-31 14:06:58 +0200

    Refactor parity function to return string instead of logging

--- #12 - Functions II/extraExercise7.js
+++ #12 - Functions II/extraExercise7.js
@@ -1,13 +1,17 @@
 function parity(number)
 {
+    let even = "Even!";
+    let odd = "Odd!";
+
     if(number % 2 === 0)
     {
-        console.log("Even!");
+        return even;
     }
     else
     {
-        console.log("Odd!");
+        return odd;
     }
 }
 
-parity(4);
+let result = parity(5);
+console.log(result);
