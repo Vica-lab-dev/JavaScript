fb0fff00bed2d8506a6312d796bd4ea3c67725cd
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-03-31 15:49:19 +0200

    Add checkFirst function to check if first element length equals given number

---
+++ #12 - Functions II/extraExercise17.js
@@ -0,0 +1,10 @@
+let list = ["viktor", "marko", "vuk"];
+
+function checkFirst(array, number)
+{
+
+    return array[0].length === number;
+}
+
+let result = checkFirst(list, 6);
+console.log(result);
