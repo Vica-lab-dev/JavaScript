a9ecdbeeee42c6aeeb457ef6d3d723f60e74cc86
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-06 20:36:13 +0200

    fetch users from API and log their names

---
+++ #14 - Canvas & API/extraExercise1.js
@@ -0,0 +1,9 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(users => {
+    users.forEach(user => {
+        console.log(user.name);
+    });
+})
+
+.catch(error => console.log(error));
