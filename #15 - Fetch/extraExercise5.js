ea92ae9a1dac2004bdc2e8427c31d34716b6ef79
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-11 10:48:48 +0200

    fetch cat breeds and display those with life span over 15 years

---
+++ #15 - Fetch/extraExercise5.js
@@ -0,0 +1,26 @@
+fetch("https://api.thecatapi.com/v1/breeds")
+.then(res => res.json())
+.then(data => {
+
+    let mainDiv = document.querySelector("#cats");
+
+    data.forEach(cat => {
+        let [min, max] = cat.life_span.split(" - ").map(Number);
+
+        if (max > 15)
+        {
+            let div = document.createElement("div");
+
+            let name = document.createElement("h3");
+            name.textContent = cat.name;
+
+            let lifeSpan = document.createElement("p");
+            lifeSpan.textContent = "Life span: " + cat.life_span + " years";
+
+            div.appendChild(name);
+            div.appendChild(lifeSpan);
+            mainDiv.appendChild(div);
+        }
+    });
+
+})
