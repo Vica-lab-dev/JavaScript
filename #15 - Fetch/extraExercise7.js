23a6f9721f7fa6b94a717e3d0bfd20de47b3b524
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-11 11:17:16 +0200

    fetch cat breeds and display names sorted alphabetically

---
+++ #15 - Fetch/extraExercise7.js
@@ -0,0 +1,14 @@
+fetch("https://api.thecatapi.com/v1/breeds")
+.then(response => response.json())
+.then(data =>
+{
+    let ul = document.querySelector("#sortedNames");
+    let names = data.filter(cat => cat.name);
+    names.sort((a, b) => a.name.localeCompare(b.name));
+    names.forEach(cat =>
+    {
+       let li = document.createElement("li");
+       li.textContent = cat.name;
+       ul.appendChild(li);
+    });
+})
