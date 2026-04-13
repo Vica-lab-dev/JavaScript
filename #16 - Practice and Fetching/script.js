17eecd79c7c95be71d8eb24c89b865905dbcb461
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-13 13:20:23 +0200

    fetch recipes and display detailed information with instructions

--- #16 - Practice and Fetching/script.js
+++ #16 - Practice and Fetching/script.js
@@ -2,13 +2,32 @@ fetch("https://dummyjson.com/recipes?limit=10&sortBy=name&order=asc")
 .then(response => response.json())
 .then(data =>
 {
-    let ul = document.querySelector("#recipes");
-
+    let mainDiv = document.querySelector("#recipes");
     data.recipes.forEach(recipe =>
     {
-        let li = document.createElement("li");
-        li.textContent = recipe.name;
-        ul.appendChild(li);
+        let recipeInstruction = document.createElement("ul");
+
+        for(let instruction of recipe.instructions)
+        {
+            let instructionElement = document.createElement("li");
+            instructionElement.innerText = instruction;
+
+            recipeInstruction.appendChild(instructionElement);
+        }
+
+        let singleRecipe = document.createElement("div");
+
+        let recipeName = document.createElement("h3");
+        recipeName.innerText = recipe.name;
+
+        let recipeCuisine = document.createElement("p");
+        recipeCuisine.innerText = recipe.cuisine;
+
+        let ingredientsList= document.createElement("li");
+        ingredientsList.innerText = recipe.ingredients;
+
+        singleRecipe.append(recipeName, recipeCuisine, recipeInstruction);
+        mainDiv.appendChild(singleRecipe);
     });
 })
-.catch(error => console.log(error));
+    .catch(error => console.log(error));
