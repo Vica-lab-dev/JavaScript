3b601f4e9beef7960f1b715b70717b8d03bd833f
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-20 23:02:00 +0200

    Add user registration form with API integration and login request

---
+++ #17 - HTTP requests/script.js
@@ -0,0 +1,56 @@
+
+for(let i = 18; i <= 100; i++)
+{
+    let ageOption = document.createElement("option");
+    ageOption.innerText = i;
+    document.querySelector("#age").append(ageOption);
+}
+
+document.querySelector("#register").addEventListener('click', function()
+{
+    let name = document.querySelector("#name").value;
+    let lastName = document.querySelector("#lastName").value;
+    let age = document.querySelector("#age").value;
+
+    if(name.trim() === "" || lastName.trim() === "")
+    {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    register(name,lastName, age);
+
+});
+
+function register(name, lastName, age)
+{
+    fetch("https://dummyjson.com/users/add", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            firstName: name,
+            lastName: lastName,
+            age: age
+        })
+    })
+        .then(response => response.json())
+    .then(data =>
+    {
+        alert("Successfully registered as "+data.firstName);
+    })
+}
+
+fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            username: 'emilys',
+            password: 'emilyspass',
+            expireInMins: 30,
+        }),
+    })
+.then(response => response.json())
+.then(data =>
+{
+    console.log(data)
+})
