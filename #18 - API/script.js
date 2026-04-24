e14f4138f17597a5bfc2ae7ce6c87668dc3d4a84
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-04-25 00:37:51 +0200

    chore: switch to ES module script and add test request for movie fetch

--- #18 - API/script.js
+++ #18 - API/script.js
@@ -1,11 +1,7 @@
 const apiUrl ="http://www.omdbapi.com/"
 
-getMovieByTitle("Terminator");
-
-async function getMovieByTitle(title){
-    let response = await callOMDbApi("t="+title);
-    console.log(response);
-}
+let response = await callOMDbApi("t=Terminator");
+console.log(response);
 
 async function callOMDbApi(params) {
     const url = apiUrl+"?apiKey="+apiKey+"&"+params;
