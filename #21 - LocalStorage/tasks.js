412f3d36e0f319fcac3e305e3efb7c36dacecbee
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-10 23:58:52 +0200

    Display tasks dynamically in DOM with delete action support

--- #21 - LocalStorage/tasks.js
+++ #21 - LocalStorage/tasks.js
@@ -5,6 +5,24 @@ if(loggedIn === null){
     window.location.href = "index.html";
 }
 
+const taskList = document.querySelector("#taskList");
+
+if(tasks.length >= 1) {
+    for(let task of tasks){
+        let singleTask = document.createElement("div");
+
+        let taskName = document.createElement("p");
+        taskName.innerText = task.name;
+
+        let deleteTask = document.createElement("button");
+        deleteTask.setAttribute("data-task-id", task.id);
+        deleteTask.innerText = "Delete task";
+
+        singleTask.append(taskName, deleteTask);
+        taskList.append(singleTask);
+    }
+}
+
 document.querySelector("#createTask").addEventListener("click", function () {
     const taskName = document.querySelector("#taskName").value;
     const dateTime = new Date();
