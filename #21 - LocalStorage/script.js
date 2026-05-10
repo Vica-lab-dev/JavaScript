7973780f4c109a539ddf36c7f1d80ad6c4c29e33
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-10 23:05:07 +0200

    Add logout functionality and improve login validation

--- #21 - LocalStorage/script.js
+++ #21 - LocalStorage/script.js
@@ -5,14 +5,20 @@ if(loggedIn === null) {
     document.querySelector("#logoutButton").style.display = "block";
 }
 
+document.querySelector("#logoutButton").addEventListener("click", function(){
+   localStorage.removeItem('loggedIn');
+   window.location.reload();
+});
+
 document.querySelector("#loginButton").addEventListener("click", function () {
     const username = document.querySelector("#username").value.trim().toLowerCase();
     const password = document.querySelector("#password").value.trim();
 
     if(username !== "admin" || password !== "123456") {
-        alert("Failed to login");
+        return alert("Failed to login");
     }
 
     localStorage.setItem("loggedIn", true);
+    window.location.reload();
 });
 
