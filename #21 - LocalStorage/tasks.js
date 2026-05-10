ee259c659ab8f12c3a66f58db79c228e8fb18892
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-10 23:37:02 +0200

    Implement task creation with unique task IDs

--- #21 - LocalStorage/tasks.js
+++ #21 - LocalStorage/tasks.js
@@ -1,5 +1,19 @@
 const loggedIn = localStorage.getItem("loggedIn");
+const tasks = [];
 
 if(loggedIn === null){
     window.location.href = "index.html";
 }
+
+document.querySelector("#createTask").addEventListener("click", function () {
+    const taskName = document.querySelector("#taskName").value;
+    const dateTime = new Date();
+    const id = dateTime.getTime();
+
+    tasks.push({
+        id: id,
+        name: taskName,
+    });
+
+    console.log(tasks);
+});
