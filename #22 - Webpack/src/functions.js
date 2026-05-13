869a323515acb434bead853f992acb4bcda1979e
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-14 00:46:22 +0200

    * Webpack

---
+++ #22 - Webpack/src/functions.js
@@ -0,0 +1,5 @@
+function HelloWorld () {
+    console.log("HelloWorld");
+}
+
+HelloWorld ();
---
+++ #22 - Webpack/src/script.js
@@ -0,0 +1,6 @@
+import { sayHello} from './user.js'
+import { faker } from "@faker-js/faker"
+
+console.log(faker.person.fullName());
+
+sayHello();
