36cd2f87a4f615fa9795f0048e648a475da8fa2b
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-15 22:19:04 +0200

    feat: add weather request error handling

--- #23 - Project I/src/script.js
+++ #23 - Project I/src/script.js
@@ -8,16 +8,21 @@ while(location.trim() === "") {
 console.log(location);
 
 //http://api.weatherapi.com/v1/current.json?key=apiKey&q=London&aqi=no
+try {
+    const response = await axios.get("http://api.weatherapi.com/v1/current.json", {
+        params: {
+            key: apiKey,
+            q: location,
+            aqi: "no"
+        }
 
-const response = await axios.get("http://api.weatherapi.com/v1/current.json", {
-    params: {
-        key: apiKey,
-        q: location,
-        aqi: "no"
+    });
+
+    if(!response.data.current.is_day){
+        document.querySelector("body").style.backgroundColor = "#232222";
     }
-
-});
-
-if(!response.data.current.is_day){
-    document.querySelector("body").style.backgroundColor = "#232222";
+} catch (error) {
+    alert("Something went wrong. Please try again.");
 }
+
+
