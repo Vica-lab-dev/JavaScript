15c59d89e7b7bd7e054e618a12ee3dda8f5d1e76
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-15 21:26:02 +0200

    Add webpack.config.js

---
+++ #23 - Project I/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: './src/script.js', // UCitava script.js
+    output: { // poslednji fajl koji ce se kreirati
+        filename: "script.min.js", // I nalazi se u dist folderu
+        path: path.resolve(__dirname, "dist"), // Nadji mi dist folder u projektu
+    }
+};
