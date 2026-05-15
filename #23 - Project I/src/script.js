83c0b0b1aace7baac8b96653f47a29ded45ad6da
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-15 22:14:40 +0200

    feat: add dynamic background color for night weather

--- #23 - Project I/src/script.js
+++ #23 - Project I/src/script.js
@@ -6,3 +6,18 @@ while(location.trim() === "") {
     location = prompt('Enter location');
 }
 console.log(location);
+
+//http://api.weatherapi.com/v1/current.json?key=apiKey&q=London&aqi=no
+
+const response = await axios.get("http://api.weatherapi.com/v1/current.json", {
+    params: {
+        key: apiKey,
+        q: location,
+        aqi: "no"
+    }
+
+});
+
+if(!response.data.current.is_day){
+    document.querySelector("body").style.backgroundColor = "#232222";
+}
