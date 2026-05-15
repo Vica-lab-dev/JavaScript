d2eb73570e2ed4a731defb03c8759682bc768509
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-15 23:19:49 +0200

    feat: update location state after user selection

--- #23 - Project I/src/script.js
+++ #23 - Project I/src/script.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { getUserLocation} from "./components/location.js";
+import {getCurrentWeatherForLocation} from "./components/weatherApi.js";
 
 let location = localStorage.getItem('location') || getUserLocation();
 localStorage.setItem('location', location);
@@ -7,21 +8,12 @@ localStorage.setItem('location', location);
 document.querySelector("#locationButton").addEventListener('click', function () {
     let askLocation = getUserLocation();
     localStorage.setItem('location', askLocation);
+    location = askLocation;
 });
 
-//http://api.weatherapi.com/v1/current.json?key=apiKey&q=London&aqi=no
-try {
-    const response = await axios.get("http://api.weatherapi.com/v1/current.json", {
-        params: {
-            key: apiKey,
-            q: location,
-            aqi: "no"
-        }
-    });
+    const response = await getCurrentWeatherForLocation(location);
+    console.log(response);
 
     if(!response.data.current.is_day){
         document.querySelector("body").style.backgroundColor = "#232222";
     }
-} catch (error) {
-    alert("Something went wrong. Please try again.");
-}
