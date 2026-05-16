4c0279bcabdffce20ba43a90c2a709a55f643455
Vica-lab-dev <vicomaletkovic@gmail.com>
2026-05-17 00:09:29 +0200

    * chore: rename webpack config to .cjs * build: add dotenv plugin support to webpack config

--- #23 - Project I/webpack.config.cjs
+++ #23 - Project I/webpack.config.cjs
@@ -1,4 +1,5 @@
 const path = require("path");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: "production",
@@ -6,5 +7,8 @@ module.exports = {
     output: { // poslednji fajl koji ce se kreirati
         filename: "script.min.js", // I nalazi se u dist folderu
         path: path.resolve(__dirname, "dist"), // Nadji mi dist folder u projektu
-    }
+    },
+    plugins: [
+        new Dotenv()
+    ]
 };
